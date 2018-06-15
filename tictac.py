a28b09880e549e5b2fe79673dac7fcf5ff3a5339
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-06-15 17:23:09 -0400

    added while loop for marker selection

--- tictac.py
+++ tictac.py
@@ -1,1 +1,32 @@
-player1 = input ("Please select 'X' or 'O' \n>>>>")
+#TicTacToe by Tyler Gunter
+
+print("Welcome to Tic Tac Toe!")
+
+#Defining the board
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+#Defining the display board function
+def display_board():
+	print('  |' + '   |')
+	print(board[1] + ' | ' + board[2] + ' | ' + board[3])
+	print('  |' + '   |')
+	print('---------')
+	print('  |' + '   |')
+	print(board[4] + ' | ' + board[5] + ' | ' + board[6])
+	print('  |' + '   |')
+	print('---------')
+	print('  |' + '   |')
+	print(board[7] + ' | ' + board[8] + ' | ' + board[9])
+	print('  |' + '   |')
+
+display_board()
+
+def player_input():
+	while True:
+		choice = input("Choose an empty space to place X.\n>>>")
+		choice = int(choice)
+
+		board[choice] = 'X'
+		display_board()
+player_input()
+
